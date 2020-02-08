2d20038552ca1a325171101436ad571c5d55d686
barnstorm3r <barnabyc@sky.com>
2020-02-08 16:44:02 +0000

    Get read_in_file working

--- gameoflife.c
+++ gameoflife.c
@@ -1,11 +1,14 @@
 #include<stdio.h>
+#include<string.h>
 #include"gol.h"
+
 int main(int argc, char argv[]){
-  
+
   FILE *gridfile = fopen("./glider.txt", "r");
   struct universe v;
   read_in_file(gridfile,&v);
-
+  
+  printf("#\n%s\n#\n%d\n#", v.grid, v.width);
 
   /* evolve(&v,will_be_alive);
   evolve(&v,will_be_alive);
--- gol.c
+++ gol.c
@@ -3,16 +3,29 @@
 #include"gol.h"
 
 void read_in_file(FILE *infile, struct universe *u) {
-    int rowLength = 512;
+    unsigned short rowLength = 512;
     char buffer[rowLength];
     char *grid = u->grid;
 
-    strcpy(grid, "");
+    fgets(buffer, 512, infile);
+    buffer[strlen(buffer)-1] = 0;
+    strcpy(grid, buffer);
+    
+    // length of a row
+    u->width = rowLength = strlen(buffer);
 
-    while(fgets(buffer, rowLength, infile)) {
-        buffer[strlen(buffer) - 1] = 0;
+    while(fgets(buffer, 512, infile)) {
+        buffer[rowLength] = 0;
         strcat(grid, buffer);
     }
 
-    printf("%s", grid);
+    fclose(infile);
+
+    int cur = 0;
+    printf("\nSuccessfully read in grid:\n");
+    while(cur < strlen(grid)) {
+        printf("%.*s\n", rowLength, grid + cur);
+        cur += rowLength;
+    }
+    printf("\n");
 }
