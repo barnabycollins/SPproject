05dcabe8062b2df0b2b6c6b5d45ae27acf8a47fb
Barnaby Collins <barnabyc@sky.com>
2020-02-20 18:08:16 +0000

    Document functions, add rule check, del print_grid

--- gol.c
+++ gol.c
@@ -8,17 +8,18 @@ int get_index(struct universe *u, int column, int row);
 int sum_surrounding(struct universe *u, int column, int row);
 int sum_surrounding_torus(struct universe *u, int column, int row);
 int check_alive(struct universe *u, int column, int row, unsigned short sur_sum);
-void print_grid(struct universe *u);
 float get_percent_alive(struct universe *u);
 int properMod(int a, int b);
 void check_alloc(char *p);
 short check_coords(struct universe *u, int column, int row);
 void error(char *message);
 
-// todo document functions
 // todo check over number types
 
-
+/**
+ * Reads the contents of the file at the given file pointer
+ * into the structure at the given universe pointer
+ */
 void read_in_file(FILE *infile, struct universe *u) {
     char buffer[513];
 
@@ -52,6 +53,10 @@ void read_in_file(FILE *infile, struct universe *u) {
     u->num_generations = 1;
 }
 
+/**
+ * Writes the structure at the given universe pointer
+ * into the file at the given file pointer
+ */
 void write_out_file(FILE *outfile, struct universe *u) {
     unsigned int cur = 0;
 
@@ -61,6 +66,12 @@ void write_out_file(FILE *outfile, struct universe *u) {
     }
 }
 
+/**
+ * Checks whether the cell at the given co-ordinates
+ * in the given universe is currently alive
+ * 
+ * Returns 1 if alive and 0 if not
+ */
 int is_alive(struct universe *u, int column, int row) {
     // if we're outside the grid, return 0
     // (when this is called in torus mode the column and row will have already been modulo'd)
@@ -69,6 +80,13 @@ int is_alive(struct universe *u, int column, int row) {
     return u->grid[get_index(u, column, row)] == '*';
 }
 
+/**
+ * Returns whether the cell at the given co-ordinates
+ * in the given universe will be alive
+ * after a (non-toroidal) evolution
+ * 
+ * Returns 1 if it will be alive, and 0 if not
+ */
 int will_be_alive(struct universe *u, int column, int row) {
     if (check_coords(u, column, row)) error("Co-ordinates lie outside the universe grid!");
     
@@ -76,13 +94,28 @@ int will_be_alive(struct universe *u, int column, int row) {
     return check_alive(u, column, row, sur_sum);
 }
 
+/**
+ * Returns whether the cell at the given co-ordinates
+ * in the given universe will be alive
+ * after a toroidal evolution
+ * 
+ * Returns 1 if it will be alive, and 0 if not
+ */
 int will_be_alive_torus(struct universe *u, int column, int row) {
     if (check_coords(u, column, row)) error("Co-ordinates lie outside the universe grid!");
-    
+
     unsigned short sur_sum = sum_surrounding_torus(u, column, row);
     return check_alive(u, column, row, sur_sum);
 }
 
+/**
+ * Evolves the universe at the given pointer using the given rule,
+ * writing the evolved universe back into the pointer
+ * 
+ * Rule should take a universe pointer, a column and row and return
+ * 1 if the cell at that location will be alive and
+ * 0 if not
+ */
 void evolve(struct universe *u, int (*rule)(struct universe *u, int column, int row)) {
     char *newgrid = malloc(strlen(u->grid)+1);
     check_alloc(newgrid);
@@ -95,7 +128,9 @@ void evolve(struct universe *u, int (*rule)(struct universe *u, int column, int 
         // for each column
         for (unsigned short j = 0; j < u->width; j++) {
             // evolve the current grid cell
-            newgrid = strcat(newgrid, (*rule)(u, j, i) ? "*" : ".");
+            int value = (*rule)(u, j, i);
+            if (value != 1 && value != 0) error("Given rule returned invalid value!");
+            newgrid = strcat(newgrid, value ? "*" : ".");
         }
     }
 
@@ -108,16 +143,34 @@ void evolve(struct universe *u, int (*rule)(struct universe *u, int column, int 
     u->num_generations += 1;
 }
 
+/**
+ * Print statistics about the structure at the given universe pointer
+ * 
+ * Prints percentage of cells currently alive
+ * and percentage of cells alive on average
+ */
 void print_statistics(struct universe *u) {
     printf("%.3f%% of cells currently alive\n%.3f%% of cells alive on average", get_percent_alive(u), u->avg_alive);
 }
 
+
 // PRIVATE FUNCTIONS
 
+/**
+ * Returns the string index at which a certain cell can be found
+ * 
+ * Gets grid width from the current universe and
+ * calculates the location of the cell in the given column and row
+ */
 int get_index(struct universe *u, int column, int row) {
     return row * u->width + column;
 }
 
+/**
+ * Finds the number of alive cells in the 8 cells
+ * surrounding the cell at the given co-ordinates
+ * in the given (non-toroidal) universe
+ */
 int sum_surrounding(struct universe *u, int column, int row) {    
     unsigned short total = 0;
 
@@ -132,6 +185,11 @@ int sum_surrounding(struct universe *u, int column, int row) {
     return total;
 }
 
+/**
+ * Finds the number of alive cells in the 8 cells
+ * surrounding the cell at the given co-ordinates
+ * in the given toroidal universe
+ */
 int sum_surrounding_torus(struct universe *u, int column, int row) {
 
     unsigned short total = 0;
@@ -147,6 +205,12 @@ int sum_surrounding_torus(struct universe *u, int column, int row) {
     return total;
 }
 
+/**
+ * Returns whether the given cell should be alive
+ * in the next generation, given:
+ * - cell column and row
+ * - number of live cells surrounding it
+ */
 int check_alive(struct universe *u, int column, int row, unsigned short sur_sum) {
     if (2 <= sur_sum && sur_sum <= 3) {
         if (sur_sum == 2 && !is_alive(u, column, row)) {
@@ -159,18 +223,10 @@ int check_alive(struct universe *u, int column, int row, unsigned short sur_sum)
     return 0;
 }
 
-void print_grid(struct universe *u) {
-    printf("\n");
-
-    unsigned int cur = 0;
-    while(cur < strlen(u->grid)) {
-        printf("%.*s\n", u->width, u->grid + cur);
-        cur += u->width;
-    }
-
-    printf("\n");
-}
-
+/**
+ * Returns the percentage of cells that are alive
+ * in the given universe
+ */
 float get_percent_alive(struct universe *u) {
     unsigned int len = strlen(u->grid);
     unsigned int total = 0;
@@ -181,20 +237,39 @@ float get_percent_alive(struct universe *u) {
     return 100.0 * ((float) total /(float) len);
 }
 
+/**
+ * 'Proper' modulo function
+ * 
+ * Used in replacement of C's % operator, which finds remainder
+ */
 int properMod(int a, int b) {
     return (a%b+b)%b;
 }
 
+/**
+ * Checks that the given string pointer was allocated successfully,
+ * and errors if not
+ */
 void check_alloc(char *p) {
     if (p == NULL) {
         error("Failed to allocate memory to store the universe grid!");
     }
 }
 
+/**
+ * Returns whether the given co-ordinates exist in the given universe
+ * (ie whether they are outside the column and/or row range defined in the universe)
+ * 
+ * 0 if they do exist
+ * 1 if they don't
+ */
 short check_coords(struct universe *u, int column, int row) {
-    return column < 0 || row < 0 || (unsigned) column >= u->width || (unsigned) row >= u->height;
+    return (column < 0 || row < 0 || (unsigned) column >= u->width || (unsigned) row >= u->height);
 }
 
+/**
+ * Prints the given error to stderr and exits
+ */
 void error(char *message) {
     fprintf(stderr, "Error: %s\n", message);
     exit(1);
